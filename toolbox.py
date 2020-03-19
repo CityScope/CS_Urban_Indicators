08eb824d261c8e97e062b3bf278a4497ed86718b
crisjf <crisjf@mit.edu>
2020-03-19 15:08:03 -0400

    setup function in indicator can be defined to take arbitrary arguments. load_module function cannot take any arguments. this will force to define object properties in setup function #6

--- toolbox.py
+++ toolbox.py
@@ -8,7 +8,7 @@ class Handler:
 	Class to handle the connection for indicators built based on data from the GEOGRID.
 
 	'''
-	def __init__(self, table_name, GEOGRID_varname = 'GEOGRIDDATA', quietly=True):
+	def __init__(self, table_name, GEOGRID_varname = 'GEOGRID', quietly=True):
 
 		self.host = 'https://cityio.media.mit.edu/'
 		self.table_name = table_name
@@ -62,7 +62,10 @@ class Handler:
 		self.indicators[indicatorName] = I
 		if test:
 			geogrid_data = self._get_grid_data()
-			self._new_value(geogrid_data,indicatorName)
+			try:
+				self._new_value(geogrid_data,indicatorName)
+			except:
+				warn('Indicator not working: '+indicatorName)
 
 	def _new_value(self,geogrid_data,indicator_name):
 		I = self.indicators[indicator_name]
@@ -75,7 +78,7 @@ class Handler:
 						json.dumps(val)
 						new_value[i] = {'value':val}
 					except:
-						warnings.warn('Indicator return invalid type:'+str(indicator_name))
+						warn('Indicator return invalid type:'+str(indicator_name))
 			return list(new_value)
 		else:
 			if not isinstance(new_value,dict):
@@ -83,7 +86,7 @@ class Handler:
 					json.dumps(new_value)
 					new_value = {'value':new_value}
 				except:
-					warnings.warn('Indicator return invalid type:'+str(indicator_name))
+					warn('Indicator return invalid type:'+str(indicator_name))
 			if ('name' not in new_value.keys()):
 				new_value['name'] = indicator_name
 			if ('category' not in new_value.keys())&(I.category is not None):
@@ -99,7 +102,10 @@ class Handler:
 			geogrid_data = self._get_grid_data()
 		new_values = []
 		for indicator_name in self.indicators:
-			new_values+= self._new_value(geogrid_data,indicator_name)
+			try:
+				new_values+= self._new_value(geogrid_data,indicator_name)
+			except:
+				warn('Indicator not working:'+str(indicator_name))
 		return new_values
 
 	def _update_indicators(self,geogrid_data):
@@ -125,10 +131,10 @@ class Handler:
 			try:
 				grid_hash_id = hashes[self.GEOGRID_varname]
 			except:
-				warnings.warn('WARNING: Table does not have a GEOGRIDDATA variable.')
+				warn('WARNING: Table does not have a '+self.GEOGRID_varname+' variable.')
 				grid_hash_id = self.grid_hash_id
 		else:
-			warnings.warn('Cant access cityIO hashes')
+			warn('Cant access cityIO hashes')
 			sleep(1)
 			grid_hash_id=self.grid_hash_id
 		return grid_hash_id
@@ -138,7 +144,7 @@ class Handler:
 		if r.status_code==200:
 			geogrid_data = r.json()
 		else:
-			warnings.warn('WARNING: Cant access GEOGRID data')
+			warn('WARNING: Cant access GEOGRID data')
 			sleep(1)
 			geogrid_data = None
 		return geogrid_data
@@ -155,7 +161,7 @@ class Handler:
 			else:
 				attempts+=1
 		if not success:
-			warnings.warn('FAILED TO RETRIEVE URL: '+url)
+			warn('FAILED TO RETRIEVE URL: '+url)
 		return r
 
 	def geogrid_data(self):
@@ -194,16 +200,18 @@ class Handler:
 				self.perform_update(grid_hash_id=grid_hash_id)
 
 class Indicator:
-	def __init__(self,name=None,category=None):
+	def __init__(self,*args,name=None,category=None,**kwargs):
 		self.name = name
 		self.category = category
 
-		self.setup()
+		self.setup(*args,**kwargs)
 		self.load_module()
 
 	def return_indicator(self,geogrid_data):
-		'''MUST return either a dictionary, a list, or a number'''
-		return None
+		'''
+		Function must return either a dictionary, a list, or a number
+		'''
+		return []
 
 	def setup(self):
 		pass

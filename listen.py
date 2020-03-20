9e95cd58ac51b191494e515099fef364d882f850
crisjf <crisjf@mit.edu>
2020-03-20 11:37:43 -0400

    Simplified code by removing names of indicators. This is redundant as names can also be returned inside return_indicator method

--- listen.py
+++ listen.py
@@ -3,8 +3,8 @@ from density_proximity_indicator import Density_Proximity
 from innovation_indicator import InnoIndicator
 
 def main():
-	density_proximity = Density_Proximity(name="density_proximity")
-	innovation        = InnoIndicator(name="innovation")
+	density_proximity = Density_Proximity()
+	innovation        = InnoIndicator()
 
 	H = Handler('corktown', quietly=False)
 	H.add_indicator(density_proximity)

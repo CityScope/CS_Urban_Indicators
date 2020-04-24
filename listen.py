6e6c846d613ad37f948edd5a48f9bc0344542b04
doorleyr <doorleyr@tcd.ie>
2020-04-24 18:02:17 +0100

    Created additional aggregate indicators.

--- listen.py
+++ listen.py
@@ -1,4 +1,4 @@
-from toolbox import Handler
+from toolbox import Handler, Indicator
 from proximity_indicator import ProxIndicator
 from innovation_indicator import InnoIndicator
 from mobility_indicator import MobilityIndicator
@@ -8,31 +8,68 @@ from economic_indicator import EconomicIndicator
 from statistics import mean
 
 def main():
+    # Individual Indicators
     I = InnoIndicator()    
-    P = ProxIndicator(name='proximity',  category_in='heatmap', table_name='corktown')
-    M = MobilityIndicator(name='mobility',  table_name='corktown')
+#    P = ProxIndicator(name='proximity',  viz_type_in='radar', table_name='corktown')
+    P_hm = ProxIndicator(name='proximity',  viz_type_in='heatmap', indicator_type_in='heatmap', table_name='corktown')
+    M = MobilityIndicator(name='mobility', table_name='corktown')
     
-#    2nd order indicators
-    E = EconomicIndicator(innovation_indicator=I, name='Economic')
+    # 2nd order  individual indicators 
+    E = EconomicIndicator(grid_to_industries=I.grid_to_industries, 
+                          industries_to_occupations=I.industries_to_occupations,
+                          name='Economic',
+                          table_name='corktown')
     
-    swb_aggregation=[{'indicator': P, 'names': [
+    H = Handler('corktown', quietly=False)
+    H.add_indicator(I)
+#    H.add_indicator(P)
+    H.add_indicator(P_hm)
+    H.add_indicator(M)
+    H.add_indicator(E)
+    
+    
+    # Composite indicators    
+    social_aggregation=[{'indicator': P_hm, 'names': [
             'Access to education', 
 #            'Access to employment',
             'Access to restaurants',
             'Access to groceries',
             'Access to parks'
             ]}]
-       
-    S=AggregateIndicator(name='Social Well-Being',
-                              indicators_to_aggregate=swb_aggregation, 
-                              agg_fun=mean)
     
-    H = Handler('corktown', quietly=False)
-    H.add_indicator(I)
-    H.add_indicator(P)
-    H.add_indicator(M)
-    H.add_indicator(E)
-    H.add_indicator(S)  
+    social_agg_ind=AggregateIndicator(name='Social Well-Being',
+                              indicators_to_aggregate=social_aggregation, 
+                              agg_fun=mean, viz_type_in='bar')
+    
+    inno_aggregation=[{'indicator': I, 'names': [
+            'District-knowledge', 
+            'City-skills',
+            'Region-funding'
+            ]}]
+    inno_agg_ind=AggregateIndicator(name='Innovation Potential',
+                              indicators_to_aggregate=inno_aggregation, 
+                              agg_fun=mean, viz_type_in='bar')
+    
+    mobility_agg=[{'indicator': M, 'names': [
+            'Mobility CO2 Performance', 
+            'Mobility Health Impacts'
+            ]}]
+    mobility_agg_ind=AggregateIndicator(name='Sustaiable Mobility',
+                              indicators_to_aggregate=mobility_agg, 
+                              agg_fun=mean, viz_type_in='bar')
+    
+    economic_agg=[{'indicator': E, 'names': [
+            'Average Earnings', 
+            'Industry Output'
+            ]}]
+    economic_agg_ind=AggregateIndicator(name='Local Economy',
+                              indicators_to_aggregate=economic_agg, 
+                              agg_fun=mean, viz_type_in='bar')
+           
+    H.add_indicator(social_agg_ind)
+    H.add_indicator(inno_agg_ind) 
+    H.add_indicator(mobility_agg_ind) 
+    H.add_indicator(economic_agg_ind) 
 
     H.listen()
 

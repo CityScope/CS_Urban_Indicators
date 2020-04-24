a4aeaa5e6cedcfeb07cad29be155209beac4cb45
doorleyr <doorleyr@tcd.ie>
2020-04-24 18:01:45 +0100

    Updated economic indicator to include average salary and total output

--- economic_indicator.py
+++ economic_indicator.py
@@ -6,21 +6,115 @@ Created on Tue Mar 31 17:48:36 2020
 @author: doorleyr
 """
 import pandas as pd
+import json
 
 from toolbox import Handler, Indicator
 from innovation_indicator import InnoIndicator
 
+def load_output_per_employee():
+    industry_ouput=pd.read_csv('./tables/innovation_data/USA_industry_ouput.csv', skiprows=1)
+    industry_ouput=industry_ouput.set_index('2017 NAICS code')
+    output_per_employee_by_naics={}
+    for ind_row, row in industry_ouput.iterrows():
+        output_per_emp=row['Sales, value of shipments, or revenue ($1,000)']/row['Number of employees']
+        if '-' in ind_row:
+            from_code, to_code=ind_row.split('-')
+            if '(' in to_code:
+                to_code=to_code.split('(')[0]
+            for code in range(int(from_code), int(to_code)+1):
+                output_per_employee_by_naics[str(code)]=output_per_emp
+        else:
+            output_per_employee_by_naics[ind_row]=output_per_emp
+#        if '(' in ind_row:
+#            ind_row=ind_row.split('(')[0]
+#        output_per_employee_by_naics[ind_row]=output_per_emp
+    return output_per_employee_by_naics
+
+def get_baseline_employees_by_naics(table_name, table_geoids):
+    employees_by_naics={}
+    wac=pd.read_csv('./tables/{}/mi_wac_S000_JT00_2017.csv.gz'.format(table_name))
+    wac['block_group']=wac.apply(lambda row: str(row['w_geocode'])[:12], axis=1)
+    wac=wac.loc[wac['block_group'].isin(table_geoids)]
+    wac_data_full_table=wac.sum(axis=0)
+    for col in wac:
+        if 'CNS' in col:
+            naics=wac_cns_to_naics[col]
+            if '-' in naics:
+                naics=naics.split('-')[0]
+            employees_by_naics[naics]=wac_data_full_table[col]
+    return employees_by_naics
+
+   
+wac_cns_to_naics={
+        'CNS01' : '11',
+        'CNS02' : '21', 
+        'CNS03' : '22',
+        'CNS04' : '23',
+        'CNS05' : '31-33',
+        'CNS06' : '42',
+        'CNS07' : '44-45',
+        'CNS08' : '48-49',
+        'CNS09' : '51',
+        'CNS10' : '52',
+        'CNS11' : '53',
+        'CNS12' : '54',
+        'CNS13' : '55',
+        'CNS14' : '56' ,
+        'CNS15' : '61',
+        'CNS16' : '62',
+        'CNS17' : '71',
+        'CNS18' : '72',
+        'CNS19' : '81',
+        'CNS20' : '92' }
+            
 class EconomicIndicator(Indicator):
     def setup(self,*args,**kwargs):
-        self.innovation_indicator=kwargs['innovation_indicator']
+        self.table_name= kwargs['table_name']
+        self.grid_to_industries=kwargs['grid_to_industries']
+        self.industries_to_occupations=kwargs['industries_to_occupations']
         self.name=kwargs['name']
+        sim_zones=json.load(open('./tables/{}/sim_zones.json'.format(self.table_name)))
+        table_geoids=[z.split('US')[1] for z in sim_zones]
+        # get the baseline num workers in district by industry NAICS code
+        self.base_industry_composition=get_baseline_employees_by_naics(self.table_name, table_geoids)
+        self.base_worker_composition=self.industries_to_occupations(self.base_industry_composition)
+
+        self.output_per_employee_by_naics=load_output_per_employee()
         salary_data=pd.read_excel('./tables/innovation_data/national_M2019_dl.xlsx')
 #        salary_data=salary_data.set_index('occ_code')
         self.code_to_salary={salary_data.iloc[i]['occ_code']: salary_data.iloc[i]['a_mean']
             for i in range(len(salary_data))}
+        
     def return_indicator(self, geogrid_data):
-        industry_composition = self.innovation_indicator.grid_to_industries(geogrid_data)
-        worker_composition   = self.innovation_indicator.industries_to_occupations(industry_composition)
+        # add new workers to baseline workers
+        new_industry_composition = self.grid_to_industries(geogrid_data)
+        new_worker_composition   = self.industries_to_occupations(new_industry_composition)
+        all_worker_composition={k: v for k,v in self.base_worker_composition.items()}
+        for code in new_worker_composition:
+            if code in all_worker_composition:
+                all_worker_composition[code]+=new_worker_composition[code]
+            else:
+                all_worker_composition[code]=new_worker_composition[code]       
+        avg_salary=self.get_avg_salary(all_worker_composition)
+        base_ouput=self.get_total_output(self.base_industry_composition)
+        new_ouput=self.get_total_output(new_industry_composition)
+        total_output=base_ouput+new_ouput
+        self.value_indicators=[{'value': min(1, avg_salary/100000), 'name': 'Average Earnings', 
+                 'viz_type': self.viz_type},
+                {'value': min(1, total_output/(2*base_ouput)), 'name': 'Industry Output', 
+                 'viz_type': self.viz_type}]
+        return self.value_indicators
+        
+    def return_baseline(self):
+        base_ouput=self.get_total_output(self.base_industry_composition)
+        base_avg_salary=self.get_avg_salary(self.base_worker_composition)
+        return [{'value': min(1, base_avg_salary/100000), 'name': 'Average Earnings', 
+                 'viz_type': self.viz_type},
+                {'value': base_ouput, 'name': 'Industry Output', 
+                 'viz_type': self.viz_type}]
+        
+            
+    def get_avg_salary(self, worker_composition):
         for occ_code in worker_composition:
             total_salary=0
             denom=0            
@@ -33,20 +127,29 @@ class EconomicIndicator(Indicator):
             weight=worker_composition[occ_code]
             total_salary+=salary*weight
             denom+=weight
-#            print('{} : {}'.format(padded_occ_code,self.code_to_salary[padded_occ_code]))
+    #            print('{} : {}'.format(padded_occ_code,self.code_to_salary[padded_occ_code]))
         avg_salary=total_salary/denom
-        return [{'value': min(1, avg_salary/100000), 'name': self.name}]
-
+        return avg_salary
+    
+    def get_total_output(self, industry_composition):
+        total_ouput=0
+        for naics in industry_composition:
+            naics_2=naics[:2]
+            if not naics_2 in ['11', '92']: # ignore agriculture and public order/safety
+                total_ouput+=industry_composition[naics]*self.output_per_employee_by_naics[naics_2]
+        return 1000*total_ouput
+    
 def main():
     I = InnoIndicator()
-    E = EconomicIndicator(innovation_indicator=I)
+    E = EconomicIndicator(grid_to_industries=I.grid_to_industries, 
+                          industries_to_occupations=I.industries_to_occupations,
+                          table_name='corktown',
+                          name='Economic')
     
     H = Handler('corktown', quietly=False)
-    H.add_indicator(I)
     H.add_indicator(E)
     
-    coefs = I.train()
-    print(coefs)
+    print(E.return_baseline())
     print(E.return_indicator(H.geogrid_data()))
     
 if __name__ == '__main__':
